e87bdf6a171c79c7f2365b853b1de909f775e903
Your Name <you@example.com>
2024-03-03 18:24:37 +0600

    All Product

--- frontend/src/components/partial/Navbar.vue
+++ frontend/src/components/partial/Navbar.vue
@@ -1,10 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/login" v-if="!isLoggedIn">Login</router-link>
-    <router-link to="/register" v-if="!isLoggedIn">Sign Up</router-link>
-    <router-link to="/add-product" v-if="isLoggedIn">Add Product</router-link> 
-    <a href="#" @click="logout" v-if="isLoggedIn">Logout</a>
-    
+  
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Expand at md</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <router-link to="/add-product" class="nav-link" v-if="isLoggedIn">Add Product</router-link> 
+          </li>
+          <li class="nav-item">
+            <router-link to="/product-list"  class="nav-link"v-if="isLoggedIn">Products</router-link> 
+          </li><li class="nav-item"> 
+            <a href="#" @click="logout" class="nav-link" v-if="isLoggedIn">Logout</a>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link" v-if="!isLoggedIn">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link" v-if="!isLoggedIn">Sign Up</router-link>
+          </li>
+          
+        </ul>
+        <form role="search">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+    </div>
   </nav>
 </template>
 
